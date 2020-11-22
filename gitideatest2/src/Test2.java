bc619b041dad0f72f10521a5389aafd80f7e40c9
qinzhen <519510316@qq.com>
2020-11-22 23:41:06 +0800

    测试第二次

---
+++ gitideatest2/src/Test2.java
@@ -0,0 +1,13 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @author qinzhen
+ * @create 2020/11/22 23:40
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
