9f7b26ab5f821c4c8a70d6bc25151980a143218c
qinzhen <519510316@qq.com>
2020-11-22 23:12:05 +0800

    测试git整合到idea上

---
+++ gittideatest/src/GitIdea.java
@@ -0,0 +1,11 @@
+/**
+ * @author qinzhen
+ * @create 2020/11/22 23:05
+ */
+public class GitIdea {
+
+    public static void main(String[] args) {
+        System.out.println("a new program");
+    }
+
+}
